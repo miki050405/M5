2ac54ac358efb0a512c85dbd70507d0fa895bd14
miki050405 <malika.usenova555@gmail.com>
2026-05-29 22:01:56 +0600

    added homework 1

---
+++ product/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Category, Product, Review
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = 'id name'.split()
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = 'id title description price category'.split()
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id text product'.split()
+
+class ReviewDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
